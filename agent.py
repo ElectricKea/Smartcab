773bee69a911c40ecdc7caa7fcb3ac48585fa3d8
Kyle <kyle.mcmillan@gmail.com>
2017-11-26 18:56:04 +0900

    Edited code for maxQ

--- agent.py
+++ agent.py
@@ -93,10 +93,20 @@ class LearningAgent(Agent):
         # Calculate the maximum Q-value of all actions for a given state
 
         max_v = self.Q[state].values()
+        keys = []
         for key, value in self.Q[state].items():
-            if value == max(max_v):
-                maxQ = key
-        
+            if max(max_v) == 0.0:       # If the max value of 0, make a list of all 0 values
+                if value == 0.0: 
+                    keys.append(key)        
+            elif value == max(max_v):   
+                keys = key              # If the max value is greater than 0 use that action
+    
+        if type(keys)==list:
+            maxQ = random.choice(keys)  # Choose a random action from the list
+            #print ("random", keys, maxQ)
+        else:
+            maxQ = keys
+            #print("set", maxQ)
         return maxQ 
 
 
